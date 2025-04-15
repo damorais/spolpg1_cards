987ea9e5506dd10d40c5d4b77bc0473fdf640c42
Daniel Morais <damorais@gmail.com>
2025-04-15 10:46:20 -0300

    Inclusão do conjunto de regras AceEleven

---
+++ src/br/edu/ifsp/spo/java/cards/rules/AceElevenScorer.java
@@ -0,0 +1,30 @@
+package br.edu.ifsp.spo.java.cards.rules;
+
+import br.edu.ifsp.spo.java.cards.items.Card;
+
+import java.util.List;
+
+public class AceElevenScorer implements Scorer{
+
+    @Override
+    public int calculateScore(List<Card> cards) {
+        var score = 0;
+
+        for(Card card : cards){
+            switch(card.getRank()){
+                case ACE -> score += 11;
+                case TWO -> score += 2;
+                case THREE -> score += 3;
+                case FOUR -> score += 4;
+                case FIVE -> score += 5;
+                case SIX -> score += 6;
+                case SEVEN -> score += 7;
+                case EIGHT -> score += 8;
+                case NINE -> score += 9;
+                case TEN, JACK, QUEEN, KING -> score += 10;
+            }
+        }
+
+        return score;
+    }
+}
